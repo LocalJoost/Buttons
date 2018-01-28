16f37edbd135c51d50f0d4b6fce4ce7a9c937c5e
Joost van Schaik <joostvanschaik@outlooik.com>
2018-01-28 18:15:14 +0100

    Added clicker

---
+++ Assets/App/Clicker.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+
+    public GameObject ObjectToShow;
+
+    private void Awake()
+    {
+        ObjectToShow.SetActive(false);
+    }
+
+    public void Click()
+    {
+        ObjectToShow.SetActive(true);
+        StartCoroutine(HideAfterTimeout());
+    }
+
+    IEnumerator HideAfterTimeout()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ObjectToShow.SetActive(false);
+    }
+}
